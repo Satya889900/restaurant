0daee3a6ce7918fa63b54867920c3287d452cc08
SP889900 <satya.prakash.fiem.cse20@teamfuture.in>
2025-10-31 23:51:36 +0530

    add node mailer

--- config/email.js
+++ config/email.js
@@ -6,8 +6,8 @@ import nodemailer from "nodemailer";
  */
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: Number(process.env.EMAIL_PORT) === 465, // true for 465
+  port: 465,
+  secure: true, // true for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
